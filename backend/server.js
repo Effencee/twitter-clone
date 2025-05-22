8841e8b52cbad91018b143c40c6bd9ab6c822396
Adrian Fraczek <adrian.frczk01@gmail.com>
2025-05-22 17:11:11 +0200

    project setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 8000");
+  connectMongoDB();
+});
