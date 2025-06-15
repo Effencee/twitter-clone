55db326b372e2c3f4f304818d8c78ae8b2cb2e5a
Adrian Fraczek <adrian.frczk01@gmail.com>
2025-06-15 20:36:03 +0200

    add routes and get user for auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+import HomeLink from "./components/common/HomeLink";
+import { fetchUser } from "./services/userService";
+import Follow from "./pages/profile/follow/Follow";
+import SelectedPost from "./components/common/SelectedPost";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: fetchUser,
+    retry: false,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
 
   return (
-    <>
+    <div className="flex max-w-7xl mx-auto mb-16 lg:mb-0">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {data && <HomeLink />}
+        {data && <Sidebar />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Routes>
+        <Route
+          path="/"
+          element={data ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!data ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!data ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={data ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username"
+          element={data ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username/followers"
+          element={data ? <Follow /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username/following"
+          element={data ? <Follow /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/:id"
+          element={data ? <SelectedPost /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      {data && <RightPanel />}
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
